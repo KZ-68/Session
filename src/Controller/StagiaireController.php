e76a056237ce0c66045eb0c655b5bb68c54e16b6
Kevin ZITNIK <zitnik.kevin@gmail.com>
2023-07-19 18:21:49 +0200

    feat : add StagiaireController file

---
+++ src/Controller/StagiaireController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\StagiaireRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class StagiaireController extends AbstractController
+{
+    #[Route('/stagiaire', name: 'app_stagiaire')]
+    public function index(StagiaireRepository $stagiaireRepository): Response
+    {
+        $stagiaires = $stagiaireRepository->findBy([], ["nom" => "ASC"]);
+        return $this->render('stagiaire/index.html.twig', [
+            'stagiaires' => $stagiaires,
+        ]);
+    }
+}
