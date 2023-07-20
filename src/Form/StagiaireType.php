3b1cb5bd22577e1a62c712e55c1da7b6521aa963
Kevin ZITNIK <zitnik.kevin@gmail.com>
2023-07-20 17:44:21 +0200

    feat: add type form builder for Stagiaire

---
+++ src/Form/StagiaireType.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Stagiaire;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class StagiaireType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control' 
+                ]
+            ])
+            ->add('prenom', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control' 
+                ]
+            ])
+            ->add('genre', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control' 
+                ]
+            ])
+            ->add('dateNaissance', DateType::class, [
+                'widget' => 'single_text',
+                'attr' => [
+                    'class' => 'form-control' 
+                ]
+            ])
+            ->add('ville', TextType::class, [
+                'required' => false,
+                'attr' => [
+                    'class' => 'form-control' 
+                ]
+            ])
+            ->add('email', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'type' => 'email'
+                ]
+            ])
+            ->add('telephone', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control' 
+                ]
+            ])
+            ->add('valider', SubmitType::class, [
+                'attr' => [
+                    'class' => 'btn btn-success' 
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Stagiaire::class,
+        ]);
+    }
+}
