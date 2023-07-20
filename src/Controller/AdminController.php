9b0a361c1d8c791881ee6dd33ab49bdd8b1e1a35
Kevin ZITNIK <zitnik.kevin@gmail.com>
2023-07-20 17:39:41 +0200

    feat: add methods for matieres and categories

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Matiere;
+use App\Entity\Categorie;
+use App\Entity\Programme;
+use App\Form\MatiereType;
+use App\Form\CategorieType;
+use App\Repository\MatiereRepository;
+use App\Repository\CategorieRepository;
+use App\Repository\ProgrammeRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class AdminController extends AbstractController
+{
+    
+    
+    #[Route('/admin', name: 'app_admin')]
+    public function index(): Response
+    {
+        
+        return $this->render('admin/index.html.twig', []);
+    }
+
+    #[Route('/admin/matiere', name: 'app_matiere')]
+    public function matieresList(MatiereRepository $matiereRepository): Response 
+    {
+        $matieres = $matiereRepository->findBy([], ["denomination" => "ASC"]);
+        return $this->render('admin/matieresList.html.twig', [
+            'matieres' => $matieres
+        ]);
+    }
+
+    #[Route('admin/matiere/new', name: 'new_matiere')]
+    #[Route('admin/matiere/{id}/edit', name: 'edit_matiere')]
+    public function new_edit_matiere(Matiere $matiere = null, Request $request, EntityManagerInterface $entityManager): Response
+    {
+        if (!$matiere) {
+            $matiere = new Matiere();
+        }
+
+        $form = $this->createForm(MatiereType::class, $matiere);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $matiere = $form->getData();
+            $entityManager->persist($matiere);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_matiere');
+        }
+
+        return $this->render('admin/new_matiere.html.twig', [
+            'formAddMatiere' => $form,
+            'edit' => $matiere->getId()
+        ]);
+    }
+
+    #[Route('/matiere/{id}/delete', name: 'delete_matiere')]
+    public function deleteMatiere(Matiere $matiere, EntityManagerInterface $entityManager) {
+        // Prépare la suppression d'une instance de l'objet 
+        $entityManager->remove($matiere);
+        // Exécute la suppression
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_matiere');
+    }
+
+    #[Route('/admin/categorie', name: 'app_categorie')]
+    public function categoriesList(CategorieRepository $categorieRepository): Response 
+    {
+        $categories = $categorieRepository->findBy([], ["titre" => "ASC"]);
+        return $this->render('admin/categoriesList.html.twig', [
+            'categories' => $categories
+        ]);
+    }
+
+    #[Route('admin/categorie/new', name: 'new_categorie')]
+    #[Route('admin/categorie/{id}/edit', name: 'edit_categorie')]
+    public function new_edit_categorie(Categorie $categorie = null, Request $request, EntityManagerInterface $entityManager): Response
+    {
+        if (!$categorie) {
+            $categorie = new Categorie();
+        }
+
+        $form = $this->createForm(CategorieType::class, $categorie);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $categorie = $form->getData();
+            $entityManager->persist($categorie);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_categorie');
+        }
+
+        return $this->render('admin/new_categorie.html.twig', [
+            'formAddCategorie' => $form,
+            'edit' => $categorie->getId()
+        ]);
+    }
+
+    #[Route('/admin/programme', name: 'app_programme')]
+    public function programmesListe(ProgrammeRepository $programmeRepository): Response 
+    {
+        $programmes = $programmeRepository->findBy([], ["duree" => "ASC"]);
+        return $this->render('admin/programmesList.html.twig', [
+            'programmes' => $programmes
+        ]);
+    }
+}
