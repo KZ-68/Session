382acd1420db832c64106a069c3357f0a03f6f36
Kevin ZITNIK <zitnik.kevin@gmail.com>
2023-07-21 17:48:24 +0200

    feat: add SessionType Form file

---
+++ src/Form/SessionType.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Session;
+use App\Entity\Formation;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+
+class SessionType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('dateDebut', DateType::class, [
+                'widget' => 'single_text',
+                'attr' => [
+                    'class' => 'form-control' 
+                ]
+            ])
+            ->add('dateFin', DateType::class, [
+                'widget' => 'single_text',
+                'attr' => [
+                    'class' => 'form-control' 
+                ]
+            ])
+            ->add('nbMax', IntegerType::class, [
+                'empty_data' => '0',
+            ])
+            ->add('formation', EntityType::class, [
+                'class' => Formation::class,
+                'choice_label' => 'intituleFormation'
+            ])
+            ->add('valider', SubmitType::class, [
+                'attr' => [
+                    'class' => 'btn btn-success' 
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Session::class,
+        ]);
+    }
+}
