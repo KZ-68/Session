368c371da4bda0ec50d867215c864d8f4fae636d
Kevin ZITNIK <zitnik.kevin@gmail.com>
2023-07-21 17:55:00 +0200

    feat: add details, new, edit and delete methods

--- src/Controller/SessionController.php
+++ src/Controller/SessionController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
-use App\Entity\Formation;
-use App\Entity\Programme;
+use App\Entity\Session;
+use App\Entity\Stagiaire;
+use App\Form\SessionType;
 use App\Repository\SessionRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -20,19 +23,56 @@ class SessionController extends AbstractController
         ]);
     }
 
-    #[Route('/session/{id}', name: 'show_programme')]
-    public function show(Programme $programme): Response 
+    #[Route('/session/new', name: 'new_session')]
+    #[Route('/session/{id}/edit', name: 'edit_session')]
+    public function new_edit_session(Session $session = null, Request $request, EntityManagerInterface $entityManager): Response
     {
-        return $this->render('session/show.html.twig', [
-            'programme' => $programme
+        if (!$session) {
+            $session = new Session();
+        }
+
+        $form = $this->createForm(SessionType::class, $session);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $session = $form->getData();
+            $entityManager->persist($session);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_session');
+        }
+
+        return $this->render('session/new_session.html.twig', [
+            'formAddSession' => $form,
+            'edit' => $session->getId()
         ]);
     }
 
-    #[Route('/session/formation/{id}', name: 'showFormation_session')]
-    public function showFormation(Formation $formation): Response 
+    #[Route('/session/{id}/showProgramme', name: 'show_programme')]
+    public function showProgramme(Session $session): Response 
     {
-        return $this->render('session/showFormation.html.twig', [
-            'formation' => $formation
+        return $this->render('session/showProgramme.html.twig', [
+            'session' => $session
+        ]);
+    }
+
+    #[Route('/session/{id}/delete', name: 'delete_session')]
+    public function deleteSession(Session $session, Stagiaire $stagiaire, EntityManagerInterface $entityManager) {
+        // Prépare la suppression d'une instance de l'objet 
+
+        $entityManager->remove($stagiaire);
+        $entityManager->remove($session);
+        // Exécute la suppression
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_session');
+    }
+
+    #[Route('/session/{id}/show', name: 'show_session')]
+    public function showSession(Session $session): Response 
+    {
+        return $this->render('session/showSession.html.twig', [
+            'session' => $session
         ]);
     }
 }
