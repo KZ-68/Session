093544075fcb9f8dd5204382041daf5707444335
Kevin ZITNIK <zitnik.kevin@gmail.com>
2023-07-24 18:01:17 +0200

    feat: add CollectionType options

--- src/Form/ProgrammesSessionType.php
+++ src/Form/ProgrammesSessionType.php
@@ -15,7 +15,18 @@ class ProgrammesSessionType extends AbstractType
     {
         $builder
         ->add("programmes", CollectionType::class, [
+            // La collection attend l'élément qu'elle entrera dans le form
+            // Ce n'est pas obligatoire que ce soit un autre form
             'entry_type' => ProgrammeType::class,
+            'prototype' => true,
+            /* On va autoriser l'ajout de nouveaux éléments dans l'entité session qui serons persisté grâce à cascade_persist 
+            sur l'élément programme */
+            // Ca va activer un data prototype qui sera un attribut html qu'on pourra manipuler en javascript
+            'allow_add' => true,
+            'allow_delete' => true,
+            'by_reference' => false, // Obligatoire car Session n'a pas de setProgramme, mais c'est Programme qui a setSession
+            // C'est Programme qui est propriété de la relation
+            // Pour éviter un mapping false on est obligé de rajouter un by_reference
             'entry_options' => [
                 'attr' => ['class' => 'form-options'],
             ],
