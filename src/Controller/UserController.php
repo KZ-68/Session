d476d67cf200c988d1fde7080e8bdfce311b6f3d
Kevin ZITNIK <zitnik.kevin@gmail.com>
2023-07-26 11:43:32 +0200

    feat: add UserController for user access paths

---
+++ src/Controller/UserController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Matiere;
+use App\Entity\Categorie;
+use App\Entity\Formation;
+use App\Repository\FormationRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class UserController extends AbstractController
+{
+    #[Route('/profile', name: 'app_user')]
+    public function index(): Response
+    {
+        return $this->render('user/index.html.twig', [
+            'controller_name' => 'UserController',
+        ]);
+    }
+
+     /* Attention : Si plusieurs view ont dans le nom de la route un identifiant et se trouve dans le même dossier
+    il faut une ajouter une sépération supplémentaire après l'identifiant */
+    #[Route('/matiere/{id}/show', name: 'show_matiere')]
+    #[IsGranted('ROLE_USER')]
+    public function showMatiere(Matiere $matiere): Response 
+    {
+        return $this->render('user/showMatiere.html.twig', [
+            'matiere' => $matiere
+        ]);
+    }
+
+    #[Route('/categorie/{id}/show', name: 'show_categorie')]
+    #[IsGranted('ROLE_USER')]
+    public function showCategorie(Categorie $categorie): Response 
+    {
+        return $this->render('user/showCategorie.html.twig', [
+            'categorie' => $categorie
+        ]);
+    }
+
+    #[Route('/formation', name: 'app_formation')]
+    #[IsGranted('ROLE_USER')]
+    public function formationsList(FormationRepository $formationRepository): Response
+    {
+        $formations = $formationRepository->findBy([], ["intituleFormation" => "ASC"]);
+        
+        return $this->render('user/index.html.twig', [
+            'formations' => $formations
+        ]);
+    }
+
+    #[Route('/formation/{id}/show', name: 'show_formation')]
+    #[IsGranted('ROLE_USER')]
+    public function showformation(Formation $formation): Response 
+    {
+        return $this->render('user/showFormation.html.twig', [
+            'formation' => $formation
+        ]);
+    }
+}
