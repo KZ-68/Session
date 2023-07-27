38c71691ba70e6f65215c1e0298cf081216a5725
Kevin ZITNIK <zitnik.kevin@gmail.com>
2023-07-27 17:58:46 +0200

    feat: add isGranted attribute for access route

--- src/Controller/SessionController.php
+++ src/Controller/SessionController.php
@@ -13,8 +13,8 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class SessionController extends AbstractController
 {
