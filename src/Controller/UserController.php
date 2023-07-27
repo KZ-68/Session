d5ce30902044aa49b4e56dc5c53041b5b97a6953
Kevin ZITNIK <zitnik.kevin@gmail.com>
2023-07-27 17:59:16 +0200

    feat: add form request for FileUploader

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,21 +2,54 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserType;
 use App\Entity\Matiere;
 use App\Entity\Categorie;
 use App\Entity\Formation;
+use App\Service\FileUploader;
+use App\Repository\UserRepository;
 use App\Repository\FormationRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class UserController extends AbstractController
 {
     #[Route('/profile', name: 'app_profile')]
-    public function index(): Response
+    public function index(Request $request, FileUploader $fileUploader, EntityManagerInterface $entityManager): Response
     {
-        return $this->render('user/index.html.twig');
+        // Récupère l'utilisateur courant
+        $user = $this->getUser();
+        // Crée le formulaire par le biais du FormType associé
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            /** @var UploadedFile $avatarFile */
+            // Récupère les données de 'avatar' venant du formulaire
+            $avatarFile = $form->get('avatar')->getData();
+            // Si avatarFile existe :
+            if ($avatarFile) {
+                // Envoie les données au Service FileUploader 
+                $avatarFileName = $fileUploader->upload($avatarFile);
+                $user->setAvatar($avatarFileName);
+                $entityManager->persist($user);
+                $entityManager->flush();
+            }
+                
+
+            return $this->redirectToRoute('app_profile');
+        }
+
+        return $this->render('user/index.html.twig', [
+            'form' => $form
+        ]);
     }
 
      /* Attention : Si plusieurs view ont dans le nom de la route un identifiant et se trouve dans le même dossier
