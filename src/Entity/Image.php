f1c9702a04b3779ad35369f9475ecb9832da436b
Kevin ZITNIK <zitnik.kevin@gmail.com>
2023-07-29 23:42:02 +0200

    feat: add Image Entity

---
+++ src/Entity/Image.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ImageRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ImageRepository::class)]
+class Image
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne(inversedBy: 'images')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Formation $formation = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $filename = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFormation(): ?Formation
+    {
+        return $this->formation;
+    }
+
+    public function setFormation(?Formation $formation): static
+    {
+        $this->formation = $formation;
+
+        return $this;
+    }
+
+    public function getFileName(): ?string
+    {
+        return $this->filename;
+    }
+
+    public function setFileName(string $filename): static
+    {
+        $this->filename = $filename;
+
+        return $this;
+    }
+}
