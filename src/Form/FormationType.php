096cdc5997d72db876627df8b3e2693d643c9f42
Kevin ZITNIK <zitnik.kevin@gmail.com>
2023-07-30 23:48:55 +0200

    feat: add FileType for multiple file + constraints

--- src/Form/FormationType.php
+++ src/Form/FormationType.php
@@ -2,12 +2,17 @@
 
 namespace App\Form;
 
+use App\Form\ImageType;
 use App\Entity\Formation;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\All;
+use Symfony\Component\Validator\Constraints\File;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 
 
 class FormationType extends AbstractType
@@ -22,6 +27,30 @@ class FormationType extends AbstractType
             ])
             ->add('description', TextType::class, [
                 'attr' => ['class' => 'form-control'],
+            ])
+            ->add('images', FileType::class, [
+                'label' => false,
+                'multiple' => true,
+                'mapped' => false,
+                'required' => false,
+                'constraints' => [
+                new All ([ 
+                    new File([
+                        'maxSize' => '2048k',
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/jpg',
+                            'image/png',
+                            'image/webp',
+                            'image/x-icon',
+                            'image/tiff',
+                            'image/bmp'
+                        ],
+                        'mimeTypesMessage' => 'Please upload a valid image format',
+                    ])
+                ])
+                    
+                ],
             ])
             ->add('valider', SubmitType::class, [
                 'attr' => [
