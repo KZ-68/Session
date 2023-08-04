c412599886fd730fec4b75a13c771fad2913fc4d
Kevin ZITNIK <zitnik.kevin@gmail.com>
2023-08-04 13:10:13 +0200

    feat: add user password change + hasher

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -7,7 +7,9 @@ use App\Form\UserType;
 use App\Entity\Matiere;
 use App\Entity\Categorie;
 use App\Entity\Formation;
+use App\Form\UserEmailType;
 use App\Service\FileUploader;
+use App\Form\UserPasswordType;
 use App\Form\UserSettingsType;
 use App\Repository\UserRepository;
 use App\Repository\FormationRepository;
@@ -19,6 +21,8 @@ use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Component\Security\Http\Attribute\CurrentUser;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 
 class UserController extends AbstractController
 {
@@ -93,27 +97,86 @@ class UserController extends AbstractController
         ]);
     }
 
-    #[Route('/profile/settings', name: 'settings_profile')]
+    #[Route('/profile/settings/{id}', name: 'settings_profile', methods:['GET', 'POST'])]
     #[IsGranted('ROLE_USER')]
-    public function changeEmail(Request $request, UserRepository $userRepository, EntityManagerInterface $entityManager): Response
+    public function editEmail(User $user, Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $hasher): Response
     {
 
-        $user = $this->getUser();
-        $form = $this->createForm(UserSettingsType::class, $user);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $email = $form->get('email')->getData();
-            
-            $userRepository->updateUser($user, $email);
-            $entityManager->persist($user);
-            $entityManager->flush();
-
-            return $this->redirectToRoute('settings_profile');
+        if(!$this->getUser()) {
+            return $this->redirectToRoute('app_login');
         }
 
+        if ($this->getUser() !== $user) {
+            return $this->redirectToRoute('app_session');
+        }
+
+        $form1name = $this->createForm(UserEmailType::class, $user);
+
+        $form2name = $this->createForm(UserPasswordType::class, $user);
+
+        if ($request->isMethod('POST')) {
+        
+            $form1name->handleRequest($request);
+        
+            if ($form1name->isSubmitted() && $form1name->isValid()) {
+                $email = $form1name->get('email')->getData();
+                    $confirmEmail = $form1name->get('confirmEmail')->getData();
+                    if($confirmEmail === $email) {
+                        $user = $form1name->getData();
+                        $entityManager->persist($user);
+                        $entityManager->flush();
+                        
+                        $this->addFlash(
+                            'success', 
+                            'Les informations ont été modifiés avec succès !'
+                        );
+            
+                        return $this->redirectToRoute('settings_profile', ['id' => $user->getId()]);
+
+                    } else {
+                        $this->addFlash(
+                            'warning', 
+                            'Les informations renseignés sont incorrects.'
+                        );
+                    }
+            } 
+            
+            $form2name->handleRequest($request);
+
+            if ($form2name->isSubmitted() && $form2name->isValid()) {
+                $oldPassword = $form2name->get('plainPassword')->getData();
+                $newPassword = $form2name->get('newPassword')->getData();
+    
+                if ($hasher->isPasswordValid($user, $oldPassword)) {
+                    $encodedPassword = $hasher->hashPassword(
+                        $user,
+                        $newPassword
+                    );
+                    $user->setPassword($encodedPassword);
+                    $entityManager->flush();
+                    
+                    $this->addFlash(
+                        'success', 
+                        'Les informations ont été modifiés avec succès !'
+                    );
+                    
+                    return $this->redirectToRoute('settings_profile', ['id' => $user->getId()]);
+
+                } else {
+                    die(dump($oldPassword));
+                    $this->addFlash(
+                        'warning', 
+                        'Les informations renseignés sont incorrects.'
+                    );
+                }
+                
+            }
+            
+        }
         return $this->render('user/settingsProfile.html.twig', [
-            'form' => $form
+            'form1name' => $form1name,
+            'form2name' => $form2name
         ]);
     }
+
 }
